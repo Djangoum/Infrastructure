0ea06af5baa3e17f278b018059d54a49979a7969
Ariel Amor <arielamorgarcia@gmail.com>
2020-12-28 20:43:16 +0100

    new aurora database

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,12 +19,11 @@ provider "aws" {
 resource "aws_db_instance" "lists_database" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = "aurora-mysql"
-  engine_version       = "5.7.12"
+  engine               = "mysql"
+  engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = local.db_server_name
   username             = "foo"
   password             = "foobarbaz"
-  parameter_group_name = "default.aurora-mysql5.7"
-  final_snapshot_identifier  = "foo"
+  parameter_group_name = "default.mysql5.7"
 }
